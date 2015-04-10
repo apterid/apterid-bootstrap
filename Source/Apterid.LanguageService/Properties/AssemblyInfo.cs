122f19d1cf888582e4f5f71288f51840966343eb
Gordon Tisher <gordon@balafon.net>
2015-04-09 21:52:29 -0700

    skeleton of a VS language service

---
+++ Source/Apterid.LanguageService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Apterid.LanguageService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Apterid")]
+[assembly: AssemblyProduct("Apterid.LanguageService")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Apterid.LanguageService_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a14d7b94811068c45d9b20b3c0f7d49defb07d7e7df87bce15c404a60b7a099fd0b0b4a406d985bf9c6192e230e48f24c266471f316e943ed3bf499bdfbb04fab4ab9ee9d7e90ae2b48c35ed085b0649a60c4b177b38d57da70a943493b4cffbf2035671f1f8fce778a7f3aa39c9e8f4ca9a6917f8902a0ab2148423169011cc")]
+[assembly: InternalsVisibleTo("Apterid.LanguageService_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a14d7b94811068c45d9b20b3c0f7d49defb07d7e7df87bce15c404a60b7a099fd0b0b4a406d985bf9c6192e230e48f24c266471f316e943ed3bf499bdfbb04fab4ab9ee9d7e90ae2b48c35ed085b0649a60c4b177b38d57da70a943493b4cffbf2035671f1f8fce778a7f3aa39c9e8f4ca9a6917f8902a0ab2148423169011cc")]
