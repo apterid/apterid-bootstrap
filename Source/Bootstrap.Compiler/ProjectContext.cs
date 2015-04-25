59d7c35e85c4e4f8106f2a3608afd7181d9f1615
Gordon Tisher <gordon@balafon.net>
2015-04-24 21:31:32 -0700

    #1: a smidgen of parser work

---
+++ Source/Bootstrap.Compiler/ProjectContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apterid.Bootstrap.Compiler
+{
+    /// <summary>
+    /// Holds information about compilation for a project (external references, project ASTs).
+    /// </summary>
+    class ProjectContext
+    {
+    }
+}
