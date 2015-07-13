4b6009769b3cb73e76262e91bf910617845ebcaa
Gordon Tisher <gordon@balafon.net>
2015-07-12 20:29:41 -0700

    missing file

---
+++ Source/Apterid.Bootstrap/Apterid.Bootstrap.Parse/Syntax/Token.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apterid.Bootstrap.Parse.Syntax
+{
+    public abstract class Token : Node
+    {
+    }
+
+    public class Keyword : Token
+    {
+    }
+
+    public class Identifier : Token
+    {
+    }
+
+    public class QualifiedIdentifier : Identifier
+    {
+        IList<Identifier> qualifiers;
+
+        public IEnumerable<Identifier> Qualifiers
+        {
+            get
+            {
+                return qualifiers ?? (qualifiers = new List<Identifier>());
+            }
+            set
+            {
+                qualifiers = value.ToList();
+            }
+        }
+    }
+}
